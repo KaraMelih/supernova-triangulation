c9cf4c0b325f3fba250c3fc7db1d4018737aa9dc
Melih Kara <kara@kit.edu>
2022-09-30 17:27:58 +0200

    speeded up

--- sn_nu_delays.py
+++ sn_nu_delays.py
@@ -55,29 +55,6 @@ class Detector:
         stheta = np.cos(codelta)
         return radius * np.array([stheta * cphi, stheta * sphi, ctheta])
 
-
-def append_at_obs(timestring, dataframe):
-    """ append x,y,z values at a given obs time
-    """
-    dataframe['ObsTime'] = timestring
-    ObsTime = Time(timestring, format='iso', scale='utc')
-    dataframe['x (m)'] = -999
-    dataframe['y (m)'] = -999
-    dataframe['z (m)'] = -999
-    for _name in detectors_df.index:
-        detector = detectors_df.loc[_name]
-        detector_obj = Detector(name=_name,
-                                lon=detector['Longitude (deg)'],
-                                lat=detector['Latitude (deg)'],
-                                height=detector['Height (m)'],
-                                sigma=detector['Time Uncertainty (s)'],
-                                bias=detector['Bias (s)'])
-        x, y, z = detector_obj.get_xyz(ObsTime)
-        detectors_df.loc[_name, 'x (m)'] = x.value
-        detectors_df.loc[_name, 'y (m)'] = y.value
-        detectors_df.loc[_name, 'z (m)'] = z.value
-
-
 def get_xyz_from_gcrs(gcrs_coor, radius=10, scale_rad=1):
     """ radius in kpc
         returns x,y,z in GCRS coor, in meters
@@ -91,7 +68,12 @@ def get_xyz_from_gcrs(gcrs_coor, radius=10, scale_rad=1):
     return radius * np.array([SNx, SNy, SNz])
 
 #------------------------------------------------ load assets ----------------------------------------------------------
+## Detectors data
+detectors_df = pd.read_csv('./assets/detector_locations.csv', index_col='Unnamed: 0')
+
+## Stars data
 stars_radec_list = np.loadtxt('./assets/star_candidates.txt', usecols=(1, 2), delimiter=',')
+star_random_radii = np.random.uniform(low=1.5, high=12, size=len(stars_radec_list))
 candid_names = np.loadtxt('./assets/star_candidates.txt', usecols=0, delimiter=',', dtype=str)
 star_names_list = []
 for _name in candid_names:
@@ -101,14 +83,17 @@ for _name in candid_names:
     else:
         star_names_list.append(stripped_name + "-2")
 
-detectors_df = pd.read_csv('./assets/detector_locations.csv', index_col='Unnamed: 0')
+_stars = np.column_stack((star_names_list, stars_radec_list, star_random_radii*Earthradkpc))
+stars_df = pd.DataFrame(_stars, columns=('Star Name', 'RA', 'DEC', 'Rand. Dist'))
+stars_df = stars_df.astype({"RA": float, "DEC": float, 'Rand. Dist':float})
+#-----------------------------------------------------------------------------------------------------------------------
 
 def get_detectors_at_time(obstime_str="2022-06-14 20:00:00.100", detectors=detectors_df):
     obstime = Time(obstime_str, format='iso', scale='utc')
     df_new = detectors.copy()
     df_new['ObsTime'] = np.repeat(obstime, len(detectors))
     df_new['x (m)'], df_new['y (m)'], df_new['z (m)'] = 0, 0, 0
-    xyz_detectors = np.zeros((len(detectors),3))
+    # xyz_detectors = np.zeros((len(detectors),3))
     for i, name in enumerate(detectors.index):
         det = detectors.loc[name]
         detobj = Detector(name=name, lon=det['Longitude (deg)'], lat=det['Latitude (deg)'],
@@ -117,69 +102,55 @@ def get_detectors_at_time(obstime_str="2022-06-14 20:00:00.100", detectors=detec
         df_new.loc[name, 'x (m)'], df_new.loc[name, 'y (m)'], df_new.loc[name, 'z (m)'] = detobj.get_xyz(obstime).value
     return df_new
 
-def get_stars_at_time(obstime_str="2022-06-14 20:00:00.100", stars_radec=stars_radec_list,
-                      radii='random', star_names=star_names_list):
-    assert len(star_names_list) == len(stars_radec), "length of Names and Coordinates don't match!"
-    random_radii = np.random.uniform(low=1.5, high=12, size=len(star_names))
-    ra = stars_radec[:, 0]
-    dec = stars_radec[:, 1]
+def get_stars_at_time(obstime_str="2022-06-14 20:00:00.100"):
     obstime = Time(obstime_str, format='iso', scale='utc')
-    skycoors = SkyCoord(ra, dec, frame="fk5", obstime=obstime, unit='deg')
-    if type(radii)==str and radii=='random':
-        radii_stars = Earthradkpc * random_radii
-    else:
-        radii_stars = np.repeat(radii, len(ra))
+    skycoors = SkyCoord(stars_df['RA'], stars_df['DEC'], frame="fk5", obstime=obstime, unit='deg')
+    radii_stars = stars_df['Rand. Dist'].values
+
     star_xyz = get_xyz_from_gcrs(skycoors.gcrs, radius=radii_stars)
-    star_dict = {'name':star_names, 'RA (deg)':ra, 'DEC (deg)':dec,
-                 'x (m)':star_xyz[0], 'y (m)':star_xyz[1], 'z (m)':star_xyz[2]}
-    star_df = pd.DataFrame.from_dict(star_dict).set_index('name')
-    return star_df
+    star_dict = {'Star Name': stars_df['Star Name'],
+                 'RA (deg)': stars_df['RA'],
+                 'DEC (deg)': stars_df['DEC'],
+                 'x (m)': star_xyz[0],
+                 'y (m)': star_xyz[1],
+                 'z (m)': star_xyz[2]}
+
+    stars_xyz = pd.DataFrame.from_dict(star_dict).set_index('Star Name')
+    return stars_xyz
+
+def star_loc_at_time(star_name, obstime_str="2022-06-14 20:00:00.100",):
+    stars_xyz = get_stars_at_time(obstime_str)
+    star = stars_xyz[stars_xyz.index==star_name]
+    return star
 
 
-def get_delays_for_time(obstime_str="2022-06-14 20:00:00.100",
-                        detectors=detectors_df,
-                        stars_radec=stars_radec_list):
-    """ For a given time, return detector delays
-        to all stars, as a dictionary contains; my_dict[star_name] => delays
+def delays_for_time_star(star_name, obstime_str="2022-06-14 20:00:00.100"):
+    """ For a given time, and given Star return detector delays
     """
-
-    stars_df = get_stars_at_time(obstime_str=obstime_str, stars_radec=stars_radec, star_names=star_names_list)
-    detectors_df = get_detectors_at_time(obstime_str="2022-06-14 20:00:00.100", detectors=detectors)
-    #     sn_candidate = stars_df.loc[star_name]
+    # at time = t get xyz of detectors and single star
+    star_df = star_loc_at_time(star_name, obstime_str)
+    detectors_df = get_detectors_at_time(obstime_str=obstime_str)
 
     title = f'{obstime_str} SN neutrino arrival delay [s]'
-
+    star_xyz = star_df.loc[star_name, ['x (m)', 'y (m)', 'z (m)']]
     delays_dict = {}
-    for star in stars_df.index:
-        delays_dict[star] = {}
-        star_xyz = stars_df.loc[star, ['x (m)', 'y (m)', 'z (m)']]
-        for detector in detectors_df.index:
-            detector_xyz = detectors_df.loc[detector, ['x (m)', 'y (m)', 'z (m)']]
-            distance = np.linalg.norm(star_xyz - detector_xyz)  # meters
-            delays_dict[star][detector] = distance / 2.9979e8  # seconds
+    for detector in detectors_df.index:
+        detector_xyz = detectors_df.loc[detector, ['x (m)', 'y (m)', 'z (m)']]
+        distance = np.linalg.norm(star_xyz - detector_xyz)  # meters
+        delays_dict[detector] = distance / 2.9979e8  # dist/light speed = seconds
 
-        delays_dict[star] = pd.DataFrame.from_dict(delays_dict[star], orient='index', columns=[title]).sort_values(
-            by=title)
-    return delays_dict
-
-
-def get_delays_for_star(star_name,
-                        delays_dict=None,
-                        obstime_str="2022-06-14 20:00:00.100",
-                        detectors=detectors_df,
-                        stars_radec=stars_radec_list):
-    if delays_dict is None:
-        delays_dict = get_delays_for_time(obstime_str=obstime_str,
-                                          detectors=detectors,
-                                          stars_radec=stars_radec)
-        star_df = delays_dict[star_name]
-    else:
-        star_df = delays_dict[star_name]
-    return star_df
+    delays_df = pd.DataFrame.from_dict([delays_dict]).T
+    nametag = star_name
+    delays_df.rename({0: nametag}, axis=1, inplace=True)
+    delays_df.sort_values(nametag, inplace=True)
+    minval = min(delays_df[nametag])
+    delays_df[nametag] = delays_df[nametag].map(lambda nametag: nametag - minval)
+    return delays_df
 
 
 ####### Make dash app
 
+### The wireframe for Earth
 lines = []
 line_marker = dict(color='cyan', width=3) # '#0066FF'
 for i, j, k in zip(xE, yE, zE):
@@ -224,18 +195,21 @@ layout = go.Layout(
 detector_text =[name + f"<br>lon: {detectors_df.loc[name, 'Longitude (deg)']:.2f}deg<br>lat: " \
                        f"{detectors_df.loc[name, 'Latitude (deg)']:.2f}deg<br>" \
                        f"Height{detectors_df.loc[name, 'Height (m)']:.2f}m" for name in detectors_df.index]
-star_text = [name + f"<br>RA:{ra:.2f}" + f"<br>DEC:{dec:.2f}" for name, ra, dec in zip(star_names_list, stars_radec_list[:, 0], stars_radec_list[:, 1])]
+
+star_text = [name +
+             f"<br>RA:{ra:.2f}" +
+             f"<br>DEC:{dec:.2f}" for name, ra, dec in zip(stars_df['Star Name'], stars_df['RA'], stars_df['DEC'])]
 
 def get_scatter_data(obstime):
     """ for a given time, get all the scatter data
     """
     detectors = get_detectors_at_time(obstime_str=obstime)
+    stars = get_stars_at_time(obstime_str=obstime)
+
     det_scat =[go.Scatter3d(x=detectors['x (m)'], y=detectors['y (m)'], z=detectors['z (m)'],
                             marker=dict(size=7, symbol='x'), text=detector_text, hoverinfo='text', mode ='markers')]
-    stars = get_stars_at_time(obstime_str=obstime)
     star_scat = [go.Scatter3d(x=stars['x (m)'], y=stars['y (m)'], z=stars['z (m)'], mode ='markers',
                               hoverinfo='text', text=star_text, marker=dict(size=5, color='white'))]
-#     delays_dict = get_delays_for_time(obstime)
     return star_scat + det_scat
 
 
@@ -321,8 +295,8 @@ def update_graph(obstime, candid_selected):
         date_string = date_object.strftime('%Y-%m-%d')+" 20:00:00.100"
     else:
         date_string = "2022-06-14 20:00:00.100"
-    all_delays = get_delays_for_time(obstime_str=date_string)
-    selected_df = get_delays_for_star(star_name, delays_dict=all_delays)
+
+    selected_df = delays_for_time_star(star_name, obstime_str=date_string)
     fig = plot_spheres(obstime, candid_name=star_name)
     selected_df['Detectors'] = selected_df.index
     selected_df = selected_df[selected_df.columns.tolist()[::-1]]
